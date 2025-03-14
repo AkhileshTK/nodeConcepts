e624c9ac27529f2f25412d638aeb9f72997bb22a
AkhileshTK <akhileshtk11@gamil.com>
2025-03-14 13:58:53 +0530

    updated of day 2

---
+++ day2/fileSystem.js
@@ -0,0 +1,23 @@
+import fs from 'fs'
+
+// utilities to deal with file system
+
+// fs.writeFileSync("./demo.txt","Gud Afternoon")
+
+
+// let data=fs.readFileSync("./demo.txt","utf-8")
+// console.log(data);
+            //   or
+// let data=fs.readFileSync("./demo.txt")
+// console.log(data.toString("utf-8"));
+
+
+
+
+// fs.appendFileSync("./demo.txt","\n gud afternoon once again")
+
+// fs.unlinkSync("./demo.txt")
+
+
+// fs.mkdirSync("./dir")
+// fs.rmdirSync("./dir")
